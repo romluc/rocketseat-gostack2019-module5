d3fa6e22c9a47e713e3e2a009149a9481227dd2d
romluc <romulofilhofilho@gmail.com>
2019-08-29 14:00:04 -0300

    Add styled components

---
+++ src/pages/Main/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FaPlus, FaGithubAlt } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositories
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Add repository" />
+        <SubmitButton disabled>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
+}
